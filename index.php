3ea441747fd4df10f24ca65658eeb5240149850a
Siam Mridha <masudalamsiam@gmail.com>
2018-03-26 00:19:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
   <title>Question 3/three</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./bootstrap-3.3.7-dist/css/bootstrap.min.css">
-  <script src="./bootstrap-3.3.7-dist/js/jquery-3.2.1.min.js"></script>
-  <script src="./bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <script src="./js/jquery-3.2.1.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
   <style>
       .cst3619_highLight{
           background-color:pink;
